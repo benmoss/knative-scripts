04741850feb195ddf807afd6a4136935ed695260
Ben Moss <benm@vmware.com>
2021-10-28 13:12:10 -0400

    add more stuff

--- install-rabbit.sh
+++ install-rabbit.sh
@@ -7,5 +7,6 @@ kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/do
 kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/latest/download/messaging-topology-operator-with-certmanager.yaml
 kubectl -n rabbitmq-system wait --timeout=5m --for=condition=Available deployments --all
 
-kubectl create -f https://github.com/knative-sandbox/eventing-rabbitmq/releases/latest/download/rabbitmq-broker.yaml
+kubectl -n knative-eventing wait --timeout 5m --for=condition=Available deployments --all
+kubectl apply -f https://github.com/knative-sandbox/eventing-rabbitmq/releases/latest/download/rabbitmq-broker.yaml
 kubectl -n knative-eventing wait --timeout=5m --for=condition=Available deployments --all
